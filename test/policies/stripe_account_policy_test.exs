419cea39d7696e854bc242587e7b028dbd26f6bd
Amy Schools <amyschools@gmail.com>
2016-11-10 11:14:08 -0800

    WIP

--- test/policies/stripe_account_policy_test.exs
+++ test/policies/stripe_account_policy_test.exs
@@ -1,146 +1,94 @@
 defmodule CodeCorps.StripeAccountPolicyTest do
   use CodeCorps.PolicyCase
 
+  import CodeCorps.StripeAccount
   import CodeCorps.StripeAccountPolicy, only: [index?: 2, show?: 2]
-  import CodeCorps.StripeAccount, only: [create_changeset: 2]
 
   alias CodeCorps.StripeAccount
 
   describe "index?" do
     test "returns true when user is an admin" do
       user = build(:user, admin: true)
-      organization = insert(:organization)
+      stripe_account = insert(:stripe_account)
 
-      assert index?(user, %{organization_id: organization.id})
+      assert index?(user, %{stripe_account_id: stripe_account.id})
     end
 
     test "returns false when user is not member of organization" do
       user = insert(:user)
-      organization = insert(:organization)
+      stripe_account = insert(:stripe_account)
 
-      refute index?(user, %{organization_id: organization.id})
+      refute index?(user, %{stripe_account_id: stripe_account.id})
     end
 
     test "returns false when user is pending member of organization" do
       user = insert(:user)
-      organization = insert(:organization)
+      stripe_account = insert(:stripe_account)
 
-      insert(:organization_membership, role: "pending", member: user, organization: organization)
+      insert(:organization_membership, role: "pending", member: user)
 
-      refute index?(user, %{organization_id: organization.id})
+      refute index?(user, %{stripe_account_id: stripe_account.id})
     end
 
     test "returns false when user is contributor of organization" do
       user = insert(:user)
-      organization = insert(:organization)
+      stripe_account = insert(:stripe_account)
 
-      insert(:organization_membership, role: "contributor", member: user, organization: organization)
+      insert(:organization_membership, role: "contributor", member: user)
 
-      refute index?(user, %{organization_id: organization.id})
+      refute index?(user, %{stripe_account_id: stripe_account.id})
     end
   end
 
   describe "show?" do
     test "returns true when user is an admin" do
       user = build(:user, admin: true)
-      organization = insert(:organization)
+      stripe_account = insert(:stripe_account)
 
-      assert show?(user, organization)
-    end
-
-    test "returns false when user is not member of organization" do
-      user = insert(:user)
-      organization = insert(:organization)
-
-      refute show?(user, organization)
-    end
-
-    test "returns false when user is pending member of organization" do
-      user = insert(:user)
-      organization = insert(:organization)
-
-      insert(:organization_membership, role: "pending", member: user, organization: organization)
-
-      refute show?(user, organization)
-    end
-
-    test "returns false when user is contributor of organization" do
-      user = insert(:user)
-      organization = insert(:organization)
-
-      insert(:organization_membership, role: "contributor", member: user, organization: organization)
-
-      refute show?(user, organization)
-    end
-
-    test "returns true when user is admin of organization" do
-      user = insert(:user)
-      organization = insert(:organization)
-
-      insert(:organization_membership, role: "admin", member: user, organization: organization)
-
-      assert show?(user, organization)
+      assert show?(user, stripe_account)
     end
 
     test "returns true when user is owner of organization" do
       user = insert(:user)
-      organization = insert(:organization)
+      stripe_account = insert(:stripe_account)
 
-      insert(:organization_membership, role: "owner", member: user, organization: organization)
+      insert(:organization_membership, role: "owner", member: user)
 
-      assert show?(user, organization)
+      assert show?(user, stripe_account)
+    end
+
+    test "returns false when user is admin of organization" do
+      user = insert(:user)
+      stripe_account = insert(:stripe_account)
+
+      insert(:organization_membership, role: "admin", member: user)
+
+      refute show?(user, stripe_account)
+    end
+
+    test "returns false when user is not member of organization" do
+      user = insert(:user)
+      stripe_account = insert(:stripe_account)
+
+      refute show?(user, stripe_account)
+    end
+
+    test "returns false when user is pending member of organization" do
+      user = insert(:user)
+      stripe_account = insert(:stripe_account)
+
+      insert(:organization_membership, role: "pending", member: user)
+
+      refute show?(user, stripe_account)
+    end
+
+    test "returns false when user is contributor of organization" do
+      user = insert(:user)
+      stripe_account = insert(:stripe_account)
+
+      insert(:organization_membership, role: "contributor", member: user)
+
+      refute show?(user, stripe_account)
     end
   end
-
-  # describe "stripe_auth?" do
-  #   test "returns true when user is an admin" do
-  #     user = build(:user, admin: true)
-  #     organization = build(:organization)
-  #
-  #     assert stripe_auth?(user, organization)
-  #   end
-  #
-  #   test "returns false when user is not member of organization" do
-  #     user = insert(:user)
-  #     organization = insert(:organization)
-  #
-  #     refute stripe_auth?(user, organization)
-  #   end
-  #
-  #   test "returns false when user is pending member of organization" do
-  #     user = insert(:user)
-  #     organization = insert(:organization)
-  #
-  #     insert(:organization_membership, role: "pending", member: user, organization: organization)
-  #
-  #     refute stripe_auth?(user, organization)
-  #   end
-  #
-  #   test "returns false when user is contributor of organization" do
-  #     user = insert(:user)
-  #     organization = insert(:organization)
-  #
-  #     insert(:organization_membership, role: "contributor", member: user, organization: organization)
-  #
-  #     refute stripe_auth?(user, organization)
-  #   end
-  #
-  #   test "returns false when user is admin of organization" do
-  #     user = insert(:user)
-  #     organization = insert(:organization)
-  #
-  #     insert(:organization_membership, role: "admin", member: user, organization: organization)
-  #
-  #     refute stripe_auth?(user, organization)
-  #   end
-  #
-  #   test "returns false when user is owner of organization" do
-  #     user = insert(:user)
-  #     organization = insert(:organization)
-  #
-  #     insert(:organization_membership, role: "owner", member: user, organization: organization)
-  #
-  #     assert stripe_auth?(user, organization)
-  #   end
-  # end
 end
--- web/policies/stripe_account_policy.ex
+++ web/policies/stripe_account_policy.ex
@@ -1,14 +1,16 @@
 defmodule CodeCorps.StripeAccountPolicy do
   import CodeCorps.Helpers.Policy,
-    only: [get_membership: 2, get_role: 1, admin_or_higher?: 1, owner?: 1]
+    only: [admin_or_higher?: 1, get_membership: 2, get_role: 1, owner?: 1]
 
   alias CodeCorps.StripeAccount
   alias CodeCorps.User
+  alias CodeCorps.OrganizationMembership
   alias Ecto.Changeset
 
   def index?(%User{admin: true}, %StripeAccount{}), do: true
   def index?(%User{} = user, %StripeAccount{} = stripe_account), do: stripe_account |> get_membership(user) |> get_role |> admin_or_higher?
 
   def show?(%User{admin: true}, %StripeAccount{}), do: true
-  def show?(%User{} = user, %StripeAccount{} = stripe_account), do: stripe_account |> get_membership(user) |> get_role |> admin_or_higher?
+  def show?(%User{} = user, %StripeAccount{} = stripe_account), do: stripe_account |> get_membership(user) |> get_role |> owner?
+  def show?(%User{}, %StripeAccount{}), do: false
 end
